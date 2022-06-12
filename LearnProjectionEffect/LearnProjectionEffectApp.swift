6406b01d43efe9baf8990d1fa9aaf8f13eb29007
n30gu1 <n@n30gu1.com>
2022-06-12 23:26:31 +0900

    Initial Commit

---
+++ LearnProjectionEffect/LearnProjectionEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnProjectionEffectApp.swift
+//  LearnProjectionEffect
+//
+//  Created by Seongheon Park on 2022/06/12.
+//
+
+import SwiftUI
+
+@main
+struct LearnProjectionEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
